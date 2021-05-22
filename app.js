23b3c5e828186c328af8c47b3ca26a7318afa75c
akash plackal <83161441+akash-plackal@users.noreply.github.com>
2021-05-22 15:35:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const ul = document.querySelector('ul')
+const btn = document.querySelector('button')
+
+
+
+async function getDadJokes() {
+    const config = {headers: {Accept : 'application/json'}} 
+    const res =  await axios.get('https://icanhazdadjoke.com/', config)
+    return res.data.joke;
+}
+
+
+
+async function addDadJokes() {
+    let joke = await getDadJokes();
+    const li = document.createElement('li')
+    li.append(joke)
+    ul.append(li)
+}
+
+btn.addEventListener('click', addDadJokes)
