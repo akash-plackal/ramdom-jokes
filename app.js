58462f30679eaeee3a73f1c0e5d2bdcd2e80d2eb
akash plackal <akashplackal@gmail.com>
2021-05-27 13:39:52 +0530

    final product ... which needs a few changes

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const div = document.querySelector("div");
+const imgDiv = document.querySelector("imgDiv");
+const p = document.querySelector("p");
 const btn = document.querySelector("button");
-document.addEventListener("DOMContentLoaded", addDadJokes);
+document.addEventListener("DOMContentLoaded", getDadJokes);
 
 async function getDadJokes() {
   const config = { headers: { Accept: "application/json" } };
   const res = await axios.get("https://icanhazdadjoke.com/", config);
-  return res.data.joke;
+  const joke = res.data.joke;
+  return (p.innerText = joke);
 }
 
-async function addDadJokes() {
-  let joke = await getDadJokes();
-  const p = document.createElement("P");
-  div.appendChild(p);
-  p.innerText = joke;
-}
+// async function addDadJokes() {
+//   let joke = await getDadJokes();
+//   const p = document.createElement("P");
+//   div.appendChild(p);
+//   p.innerText = joke;
+// }
 
-btn.addEventListener("click", addDadJokes);
+btn.addEventListener("click", getDadJokes);
